e1dfd62e4e514032b2854b6261af43db5a612eb4
asifalam232 <61245245+asifalam232@users.noreply.github.com>
2020-02-20 20:34:24 +0000

    Add files via upload
    i restructured the whole project and added hunter, wolves, plants, grass

--- Actor.java
+++ Actor.java
@@ -6,86 +6,19 @@ import java.util.List;
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29 (2)
  */
-public abstract class Actor
+public interface Actor
 {
-    // Whether the actor is alive or not.
-    private boolean alive;
-    // The actor's field.
-    private Field field;
-    // The actor's position in the field.
-    private Location location;
+    /**
+     * Make this actor do whatever it wants to do
+     * @param newActors list to recieve newly born animals
+     */
+    
+    void act (List<Actor> newActors);
     
     /**
-     * Create a new actor at location in field.
-     * 
-     * @param field The field currently occupied.
-     * @param location The location within the field.
+     * place the animal at the new location in the given field
+     * @param newLocation the animals new location.
      */
-    public Actor(Field field, Location location)
-    {
-        alive = true;
-        this.field = field;
-        setLocation(location);
-    }
+    void setLocation (Location newLocation);
     
-    /**
-     * Make this actor act - that is: make it do
-     * whatever it wants/needs to do.
-     * @param newActors A list to receive newly born actors.
-     */
-    abstract public void act(List<Actor> newActors);
-
-    /**
-     * Check whether the actor is alive or not.
-     * @return true if the actor is still alive.
-     */
-    protected boolean isAlive()
-    {
-        return alive;
-    }
-
-    /**
-     * Indicate that the actor is no longer alive.
-     * It is removed from the field.
-     */
-    protected void setDead()
-    {
-        alive = false;
-        if(location != null) {
-            field.clear(location);
-            location = null;
-            field = null;
-        }
-    }
-
-    /**
-     * Return the actor's location.
-     * @return The actor's location.
-     */
-    protected Location getLocation()
-    {
-        return location;
-    }
-    
-    /**
-     * Place the actor at the new location in the given field.
-     * @param newLocation The actor's new location.
-     */
-    protected void setLocation(Location newLocation)
-    {
-        if(location != null) {
-            field.clear(location);
-        }
-        location = newLocation;
-        field.place(this, newLocation);
-    }
-    
-    /**
-     * Return the actor's field.
-     * @return The actor's field.
-     */
-    protected Field getField()
-    {
-        return field;
-    }
 }
--- Rabbit.java
+++ Rabbit.java
@@ -1,17 +1,26 @@
 import java.util.List;
 import java.util.Random;
+import java.util.Iterator;
 
 /**
  * A simple model of a rabbit.
  * Rabbits age, move, breed, and die.
- * 
- * @author David J. Barnes and Michael Kölling
- * @version 2016.02.29 (2)
  */
 public class Rabbit extends Animal
 {
+    // Characteristics shared by all rabbits (class variables).
 
-
+    // The age at which a rabbit can start to breed.
+    private static int BREEDING_AGE = 5;
+    // The age to which a rabbit can live.
+    private static int MAX_AGE = 40;
+    // The likelihood of a rabbit breeding.
+    private static double BREEDING_PROBABILITY = 0.12;
+    // The maximum number of births.
+    private static int MAX_LITTER_SIZE = 4;
+    // food value from grass
+    private static final int GRASS_FOOD_VALUE = 6;
+    
     /**
      * Create a new rabbit. A rabbit may be created with age
      * zero (a new born) or with a random age.
@@ -20,27 +29,32 @@ public class Rabbit extends Animal
      * @param field The field currently occupied.
      * @param location The location within the field.
      */
-    public Rabbit(boolean randomAge, Field field, Location location, Gender gender)
+    public Rabbit(boolean randomAge, Field field, Location location)
     {
-        super(field, location, gender);
-        age = 0;
+        super(field, location);
+        setAge(0);
         if(randomAge) {
-            age = rand.nextInt(MAX_AGE);
+        	setAge(getRandom().nextInt(MAX_AGE));
         }
     }
-    
+      
     /**
      * This is what the rabbit does most of the time - it runs 
      * around. Sometimes it will breed or die of old age.
      * @param newRabbits A list to return newly born rabbits.
      */
-    public void act(List<Animal> newRabbits)
+    public void act(List<Actor> newRabbits)
     {
         incrementAge();
         if(isAlive()) {
             giveBirth(newRabbits);            
-            // Try to move into a free location.
-            Location newLocation = getField().freeAdjacentLocation(getLocation());
+            // Move towards a source of food if found.
+            Location newLocation = findFood();
+            if(newLocation == null) { 
+                // No food found - try to move to a free location.
+                newLocation = getField().freeAdjacentLocation(getLocation());
+            }
+            // See if it was possible to move.
             if(newLocation != null) {
                 setLocation(newLocation);
             }
@@ -52,23 +66,11 @@ public class Rabbit extends Animal
     }
 
     /**
-     * Increase the age.
-     * This could result in the rabbit's death.
-     */
-    private void incrementAge()
-    {
-        age++;
-        if(age > MAX_AGE) {
-            setDead();
-        }
-    }
-    
-    /**
      * Check whether or not this rabbit is to give birth at this step.
      * New births will be made into free adjacent locations.
      * @param newRabbits A list to return newly born rabbits.
      */
-    private void giveBirth(List<Animal> newRabbits)
+    private void giveBirth(List<Actor> newRabbits)
     {
         // New rabbits are born into adjacent locations.
         // Get a list of adjacent free locations.
@@ -78,30 +80,113 @@ public class Rabbit extends Animal
         for(int b = 0; b < births && free.size() > 0; b++) {
             Location loc = free.remove(0);
             Rabbit young = new Rabbit(false, field, loc);
-            newRabbits.add(young);
+            newRabbits.add(young);					
         }
-    }
-        
-    /**
-     * Generate a number representing the number of births,
-     * if it can breed.
-     * @return The number of births (may be zero).
-     */
-    private int breed()
-    {
-        int births = 0;
-        if(canBreed() && rand.nextDouble() <= BREEDING_PROBABILITY) {
-            births = rand.nextInt(MAX_LITTER_SIZE) + 1;
-        }
-        return births;
     }
 
+    /**
+     * Look for grass adjacent to the current location.
+     * Only the first live grass is eaten.
+     * @return Where food was found, or null if it wasn't.
+     */
+    private Location findFood()
+    {
+        Field field = getField();
+        List<Location> adjacent = field.adjacentLocations(getLocation());
+        Iterator<Location> it = adjacent.iterator();
+        while(it.hasNext()) {
+            Location where = it.next();
+            Object actor = field.getObjectAt(where);
+            if(actor instanceof Grass) {
+                Grass grass = (Grass) actor;
+                if(grass.isAlive()) { 
+                    grass.setDead();
+                    setFoodLevel(GRASS_FOOD_VALUE);
+                    return where;
+                }
+            }
+        }
+        return null;
+    }
+    
     /**
      * A rabbit can breed if it has reached the breeding age.
      * @return true if the rabbit can breed, false otherwise.
      */
-    private boolean canBreed()
+    protected boolean canBreed()
     {
-        return age >= BREEDING_AGE;
+        return getAge() >= getBreedingAge();
+    }
+    
+    /**
+     * Getter om BREEDING_AGE op te halen
+     */
+    protected int getBreedingAge()
+    {
+    	return BREEDING_AGE;
+    }
+    
+    /**
+     * returns the maximum age of a rabbit can live
+     * @return int maximum age of a rabbit can live
+     */
+    protected int getMaxAge()
+    {
+    	return MAX_AGE;
+    }
+    
+    /**
+     * Getter om BREEDING_PROBABILITY op te halen
+     * @return BREEDING_PROBABILITY breeding kansen
+     */
+    protected double getBreedingProbability()
+    {
+    	return BREEDING_PROBABILITY;
+    }
+    
+    /**
+     * Getter om MAX_LITTER_SIZE op te halen
+     * @return MAX_LITTER_SIZE maximum litter
+     */
+    protected int getMaxLitterSize()
+    {
+    	return MAX_LITTER_SIZE;
+    }
+    
+
+    /**
+     * sets a new breeding age
+     * @param newBREEDING_AGE
+     */
+    public static void setBreedingAge(int newBREEDING_AGE)
+    {
+    	BREEDING_AGE = newBREEDING_AGE;
+    }
+    
+    /**
+     * sets a new max age
+     * @param newMAX_AGE
+     */
+    public static void setMaxAge(int newMAX_AGE)
+    {
+    	MAX_AGE = newMAX_AGE;
+    }
+    
+    /**
+     * sets a new breeding probability
+     * @param newBREEDING_PROBABILITY
+     */
+    public static void setBreedingProbability(double newBREEDING_PROBABILITY)
+    {
+    	BREEDING_PROBABILITY = newBREEDING_PROBABILITY;
+    }
+    
+    /**
+     * sets a new max litter size
+     * @param newMAX_LITTER_SIZE
+     */
+    public static void setMaxLitterSize(int newMAX_LITTER_SIZE)
+    {
+    	MAX_LITTER_SIZE = newMAX_LITTER_SIZE;
     }
 }
